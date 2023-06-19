db3a3161fc1223d762356cf1a45361ca1acd202b
Simina <coneac@gmail.com>
2023-06-19 21:05:55 +0300

    todo app

--- intermediate/todo-app/index.js
+++ intermediate/todo-app/index.js
@@ -1,5 +1,12 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove,
+    update
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-a412d-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -20,6 +27,14 @@ const todoList = document.getElementById('todoList');
 const btnItemsLeft = document.getElementById('btn-items-left');
 const btnClearCompleted = document.getElementById('btn-clear-completed');
 const btnDeleteTodos = document.getElementsByClassName('todo-delete');
+const btnUpdateTodos = document.getElementsByClassName('todo-checked');
+
+const btnFilterAll = document.getElementById('btnFilterAll');
+const btnFilterActive = document.getElementById('btnFilterActive');
+const btnFilterCompleted = document.getElementById('btnFilterCompleted');
+
+let itemsArray = [];
+let currentFilter = 'All';
 
 btnTheme.addEventListener('click', () => {
     if (chkDark.checked) {
@@ -40,29 +55,57 @@ newTodoInput.addEventListener('keydown', (key) => {
     }
 });
 
+btnFilterAll.addEventListener('click', (event) => changeFilter(event));
+btnFilterActive.addEventListener('click', (event) => changeFilter(event));
+btnFilterCompleted.addEventListener('click', (event) => changeFilter(event));
+
+function changeFilter(event) {
+    let currentFilter = getCurrentFilter();
+
+    if (currentFilter == "All") {
+        btnFilterAll.classList.toggle('active');
+        event.target.classList.toggle('active');
+    }
+
+    if (currentFilter == "Active") {
+        btnFilterActive.classList.toggle('active');
+        event.target.classList.toggle('active');
+    }
+
+    if (currentFilter == "Completed") {
+        btnFilterCompleted.classList.toggle('active');
+        event.target.classList.toggle('active');
+    }
+
+    populateTodoList(itemsArray, event.target.id.replace('btnFilter', ''));
+}
+
+
 onValue(todoListInDB, function (snapshot) {
-    clearTodoList()
+    clearTodoList();
     if (!snapshot.exists()) {
         todoList.innerHTML = 'No items here .. yet!'
         return;
     }
+    itemsArray = Object.entries(snapshot.val())
+    currentFilter = getCurrentFilter();
 
-    let itemsArray = Object.entries(snapshot.val())
-    let itemsLeft = 0;
+    populateTodoList(itemsArray, currentFilter);
+})
 
-    let listHtml = ``;
-    for (let item of itemsArray) {
-        let itemId = item[0];
-        let itemValue = item[1];
-        let todoText = itemValue["todo-text"];
-        let checked = !itemValue["active"] ? "checked" : "";
-        if (!checked) itemsLeft++;
+function getCurrentFilter() {
+    if (btnFilterAll.classList.contains('active')) return "All";
+    if (btnFilterActive.classList.contains('active')) return "Active";
+    if (btnFilterCompleted.classList.contains('active')) return "Completed";
+    return "All";
+}
 
-        listHtml += `
+function addTodoItem(itemId, checked, todoText) {
+    return `
          <li class="top-rounded-border">
             <div class="todo-item top-rounded-border">
                 <div class="parent">
-                    <input type="checkbox" ${checked}>
+                    <input class="todo-checked" type="checkbox" id="${itemId}" ${checked}>
                         <img src="images/icon-check.svg" class="child hidden" style="width: 11px; height: 9px;" alt="complete todo" />
                 </div>
                 <input class="todo-input top-rounded-border" id="${itemId}" type="text" value="${todoText}">
@@ -70,18 +113,54 @@ onValue(todoListInDB, function (snapshot) {
             </div>
          </li>
         `;
+}
+
+function populateTodoList(itemsArray, currentFilter) {
+    let itemsLeft = 0;
+    let listHtml = ``;
+
+    for (let item of itemsArray) {
+        let itemId = item[0];
+        let itemValue = item[1];
+        let todoText = itemValue["todo-text"];
+        let checked = !itemValue["active"] ? "checked" : "";
+        if (!checked && (currentFilter == "All" || currentFilter == "Active")) itemsLeft++;
+
+        if (currentFilter == "All")
+            listHtml += addTodoItem(itemId, checked, todoText);
+
+        if (currentFilter == "Active" && !checked)
+            listHtml += addTodoItem(itemId, checked, todoText);
+
+        if (currentFilter == "Completed" && checked)
+            listHtml += addTodoItem(itemId, checked, todoText);
     }
-    listHtml = `<ul>` + listHtml + `</ul>`;
-    todoList.innerHTML = listHtml;
+
+    todoList.innerHTML = `<ul>` + listHtml + `</ul>`;
 
     btnItemsLeft.innerHTML = `${itemsLeft} items left`;
-    for (let deletBtn of btnDeleteTodos) {
-        deletBtn.addEventListener("click", () => {
-            let exactLocationFromDB = ref(database, `todoList/${deletBtn.id}`);
+
+    addDeleteFunctionality();
+    addUpdateFunctionality();
+}
+
+function addUpdateFunctionality() {
+    for (let updateBtn of btnUpdateTodos) {
+        updateBtn.addEventListener("click", () => {
+            let exactLocationFromDB = ref(database, `todoList/${updateBtn.id}`);
+            update(exactLocationFromDB, {active: !updateBtn.checked});
+        });
+    }
+}
+
+function addDeleteFunctionality() {
+    for (let deleteBtn of btnDeleteTodos) {
+        deleteBtn.addEventListener("click", () => {
+            let exactLocationFromDB = ref(database, `todoList/${deleteBtn.id}`);
             remove(exactLocationFromDB);
         });
     }
-})
+}
 
 function clearTodoList() {
     todoList.innerHTML = ""
