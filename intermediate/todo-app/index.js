a2311f4e63b47ee1d5e085e86544ee19378303ad
Simina <coneac@gmail.com>
2023-06-19 18:29:37 +0300

    todo app

--- intermediate/todo-app/index.js
+++ intermediate/todo-app/index.js
@@ -1,50 +1,90 @@
-const btnThemeSwitch = document.getElementById('btn-theme')
-const btnFilterAll = document.getElementById("btnFilterAll");
-const btnFilterActive = document.getElementById("btnFilterActive");
-const btnFilterCompleted = document.getElementById("btnFilterCompleted");
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const filters = {
-    All: 0,
-    Active: 1,
-    Completed: 2
+const appSettings = {
+    databaseURL: "https://realtime-database-a412d-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-btnThemeSwitch.addEventListener('click', switchTheme);
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const todoListInDB = ref(database, "todoList")
 
-const storeTheme = function (theme) {
-    localStorage.setItem('theme', theme);
+const btnTheme = document.getElementById('btn-theme');
+const chkDark = document.getElementById('dark');
+const chkLight = document.getElementById('light');
+
+const newTodoCheck = document.getElementById('new-todo-check');
+const newTodoInput = document.getElementById('new-todo');
+
+const todoList = document.getElementById('todoList');
+const btnItemsLeft = document.getElementById('btn-items-left');
+const btnClearCompleted = document.getElementById('btn-clear-completed');
+const btnDeleteTodos = document.getElementsByClassName('todo-delete');
+
+btnTheme.addEventListener('click', () => {
+    if (chkDark.checked) {
+        chkLight.checked = true;
+    } else {
+        chkDark.checked = true;
+    }
+});
+
+newTodoInput.addEventListener('keydown', (key) => {
+    if (key['key'] === 'Enter') {
+        let inputValue = newTodoInput.value;
+
+        if (inputValue) {
+            push(todoListInDB, {"todo-text": inputValue, "active": !newTodoCheck.checked})
+            clearInputField();
+        }
+    }
+});
+
+onValue(todoListInDB, function (snapshot) {
+    clearTodoList()
+    if (!snapshot.exists()) {
+        todoList.innerHTML = 'No items here .. yet!'
+        return;
+    }
+
+    let itemsArray = Object.entries(snapshot.val())
+
+    let listHtml = ``;
+    for (let item of itemsArray) {
+        let itemId = item[0];
+        let itemValue = item[1];
+        let todoText = itemValue["todo-text"];
+        let checked = !itemValue["active"] ? "checked" : "";
+
+        listHtml += `
+         <li class="top-rounded-border">
+            <div class="todo-item top-rounded-border">
+                <div class="parent">
+                    <input type="checkbox" ${checked}>
+                        <img src="images/icon-check.svg" class="child hidden" style="width: 11px; height: 9px;" alt="complete todo" />
+                </div>
+                <input class="todo-input top-rounded-border" id="${itemId}" type="text" value="${todoText}">
+                <img class="todo-delete" src="images/icon-cross.svg" style="width: 11px; height: 9px;" id="${itemId}" alt="delete todo"/>
+            </div>
+         </li>
+        `;
+    }
+    listHtml = `<ul>` + listHtml + `</ul>`;
+    todoList.innerHTML = listHtml;
+
+    btnItemsLeft.innerHTML = `0 items left`;
+    for (let deletBtn of btnDeleteTodos) {
+        deletBtn.addEventListener("click", () => {
+            let exactLocationFromDB = ref(database, `todoList/${deletBtn.id}`);
+            remove(exactLocationFromDB);
+        });
+    }
+})
+
+function clearTodoList() {
+    todoList.innerHTML = ""
 }
 
-function switchTheme() {
-    console.log('switch theme')
-    if (btnThemeSwitch.classList.contains('img-theme-light')) {
-        console.log('is light')
-        btnThemeSwitch.classList.toggle('img-theme-light');
-        // btnThemeSwitch.classList.add('img-theme-dark');
-    }
-
-    if (btnThemeSwitch.classList.contains('img-theme-dark')) {
-        console.log('is dark')
-        btnThemeSwitch.classList.toggle('img-theme-dark');
-        // btnThemeSwitch.classList.add('img-theme-light');
-    }
-}
-
-btnFilterAll.addEventListener('click', filter(filters.All));
-btnFilterAll.addEventListener('click', filter(filters.Active));
-btnFilterAll.addEventListener('click', filter(filters.Completed));
-
-function filter(type) {
-    switch (type) {
-        case filters.All:
-            btnFilterAll.classList.toggle("active");
-            btnFilterActive.classList.toggle("active");
-            btnFilterCompleted.classList.toggle("active");
-            break;
-        case filters.Active:
-            break;
-        case filters.Completed:
-            break;
-        default:
-    }
+function clearInputField() {
+    newTodoInput.value = ""
 }
