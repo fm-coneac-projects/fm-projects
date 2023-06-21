f9c9b3d7dee04e4e4a1406a684e23d601a1061d0
Simina <coneac@gmail.com>
2023-06-21 12:50:52 +0300

    add tic tac toe game functionality

--- intermediate/tic-tac-toe/index.js
+++ intermediate/tic-tac-toe/index.js
@@ -3,14 +3,11 @@
 // 3. start new game
 // 4. toggle menu
 
-//
 // Your users should be able to:
-//
-// View the optimal layout for the game depending on their device's screen size
-// See hover states for all interactive elements on the page
+
 // Play the game either solo vs the computer or multiplayer against another person
 // Bonus 1: Save the game state in the browser so that it’s preserved if the player refreshes their browser
-// Bonus 2: Instead of having the computer randomly make their moves, try making it clever so it’s proactive in blocking your moves and trying to win
+// Bonus 2: Instead of having the computer randomly make their moves, try making it clever, so it’s proactive in blocking your moves and trying to win
 // Expected behaviour
 // You can choose to make the default screen either the new game menu or the solo player game board. Note that we're using the solo player game board for the design screenshot, so if you choose the new game menu it won't match up in the design comparison slider. This isn't a big deal, but is something worth considering.
 // On the new game screen, whichever mark isn't selected for the first player is automatically assigned to the second player when the game is started.
@@ -18,15 +15,21 @@
 //     After a round, if the player chooses to quit the game, they should be taken back to the new game menu.
 //     If the restart icon in the top right is clicked, the "Restart game?" modal should show and allow the player to reset the game or cancel and continue to play.
 
-console.log("hello ")
-
-const btnX = document.getElementById('btn-x');
-const btnO = document.getElementById('btn-o');
-
-function toggleSilverButton() {
-    btnX.classList.toggle('btn-secondary-silver');
-    btnO.classList.toggle('btn-secondary-silver');
+const App = {
+    el: {
+        newGame: {
+            btnX: document.getElementById('btn-x'),
+            btnO: document.getElementById('btn-o'),
+            btnNewGameVsCPU: document.getElementById('btn-new-game-vs-cpu'),
+            btnNewGameVsPlayer: document.getElementById('btn-new-game-vs-player')
+        }
+    }
 }
 
-btnX.addEventListener('click', toggleSilverButton);
-btnO.addEventListener('click', toggleSilverButton);
+function toggleSilverButton() {
+    App.el.newGame.btnX.classList.toggle('btn-secondary-silver');
+    App.el.newGame.btnO.classList.toggle('btn-secondary-silver');
+}
+
+App.el.newGame.btnX.addEventListener('click', toggleSilverButton);
+App.el.newGame.btnO.addEventListener('click', toggleSilverButton);
